e1efbcd7abed5f2093d1643e54a59eb3a666df91
annasohromova <120181353+annasohromova@users.noreply.github.com>
2022-12-21 10:23:37 +0200

    Update Tingimuslik_opetaaror_IF.py

--- Tingimuslik_opetaaror_IF.py
+++ Tingimuslik_opetaaror_IF.py
@@ -74,3 +74,131 @@ try:
 except:
     print("Error")
 
+
+    #8
+
+#try:
+
+#    a=int(input("Kas soovite piima osta?(jah-1 või ei-0): "))
+#    b=int(input("Kas soovite saia osta?(jah-1 või ei-0): "))
+#    c=int(input("Kas soovite leiba osta?(jah-1 või ei-0): "))
+#    if a<0 and b<0 and c<0:
+#        print("Error")
+#    if a==1 and b==0 and c==0:
+#        piim=0.79
+#        sai=0
+#        leib=0
+#        S=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",S)
+#    elif a==0 and b==1 and c==0:
+#        piim=0
+#        sai=0.8
+#        leib=0
+#        P=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",P)
+#    elif a==0 and b==0 and c==1:
+#        piim=0
+#        sai=0
+#        leib=0.8
+#        F=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",F)
+#    elif a==1 and b==1 and c==0:
+#        piim=0.79
+#        sai=0.8
+#        leib=0
+#        L=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",L)
+#    elif a==0 and b==1 and c==1:
+#        piim=0
+#        sai=0.8
+#        leib=0.8
+#        O=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",O)
+#    elif a==1 and b==0 and c==1:
+#        piim=0.79
+#        sai=0
+#        leib=0.8
+#        U=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",U)
+#    elif a==1 and b==1 and c==1:
+#        piim=0.79
+#        sai=0.8
+#        leib=0.8
+#        A=piim+sai+leib
+#        print("Kõik ostetud asjad maksavad",A)
+#except:
+#    print("Vale Andmetüüp")
+
+#9
+
+#try:
+#    a=float(input("ütle pool a "))
+#    b=float(input("ütle pool b "))
+#    if a==b:
+#        print("See on ruut")
+#    else:
+#        print("See ei ole ruut")
+#except:
+#    print("Value Error")
+
+#10
+
+#try:
+#    a=float(input("1 number "))
+#    b=float(input("1 number "))
+#    c=input("mis märk sa oled +-*/ \n ")
+#    if c==("+"):
+#        print(a+b)
+#    elif c==("-"):
+#        print(a-b)
+#    elif c==("*"):
+#        print(a*b)
+#    elif c==("/"):
+#        if b==0:
+#            print("Väiksem kui 0")
+#        else:
+#            print(a/b)
+#except:
+#    print("Value Error")
+
+#11
+
+#now = datetime.datetime.now()
+#try:
+#    a=int(input("Sisesta sünniaasta. "))
+#except:
+#    print("Value Error")
+#b=int(now.year)
+#c=int(b-a)
+#print(c)
+#f=c%5
+#if f==0:
+#    print("teil on juubel")
+#else:
+#    print("Kui kahju")
+
+#12
+
+#try:
+#    a=float(input("sisesta toote hind "))
+#    if a<=10:
+#        print("sul on soodus 10%",a-a*0.1)
+#    elif a>10:
+#        print("sul on soodus 20%",a-a*0.2)
+#except:
+#    print("Value Error")
+
+
+
+#13
+
+try:
+    a=int(input("Kas sa oled mees?(jah-1 või ei-0) \n"))
+    if a==1:
+        b=int(input("Kui vana sa oled? "))
+        if b>=16 and b<=18:
+            print("sobid")
+    else:
+        print("sa oled naine sest, et sa ei sobi")
+except:
+    print("Value Error")
